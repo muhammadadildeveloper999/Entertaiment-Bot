f628d928e04fc9149b835e3243eaca862db5114b
muhammadadildeveloper999 <121722059+muhammadadildeveloper999@users.noreply.github.com>
2024-02-09 14:22:46 +0500

    Update info.py

--- info.py
+++ info.py
@@ -86,42 +86,28 @@ MY_USERS = (my_users) if my_users else []
 
 
 # Online Stream and Download
-from os import environ
-
-# Retrieve the PORT environment variable or default to 8080
-PORT = int(environ.get('PORT', 8000))
-
-# Check if the application is running on Heroku environment
+PORT = int(environ.get('PORT', 8080))  # Default to 8000 for worker dyno
 ON_HEROKU = 'DYNO' in environ
-
-# Define the bind address
 BIND_ADDRESS = environ.get('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')
-
-# Define the fully qualified domain name (FQDN)
 FQDN = environ.get('FQDN', '{}.herokuapp.com'.format(environ.get('APP_NAME'))) if ON_HEROKU else BIND_ADDRESS
-
-# Define the URL based on whether the app is running on Heroku or locally
 URL = "https://{}/".format(FQDN) if ON_HEROKU else "http://{}:{}/".format(FQDN, PORT)
-
-# Define other configurations
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
+WORKERS = int(environ.get('WORKERS', '4'))  # Adjust workers based on your requirements
 SESSION_NAME = environ.get('SESSION_NAME', 'LazyBot')
 MULTI_CLIENT = False
 name = environ.get('name', 'LazyPrincess')
 PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-
-# Check if SSL is enabled
 HAS_SSL = bool(environ.get('HAS_SSL', False))
-
-# Update URL based on SSL configuration
 if HAS_SSL:
     URL = "https://{}".format(FQDN)
 else:
     URL = "http://{}".format(FQDN)
-
-# Define the repository owner
 REPO_OWNER = "LazyDeveloperr"
+
+# Additions for worker dyno
+LAZY_MODE = is_enabled(environ.get("LAZY_MODE", "False"), False)
+LAZY_RENAMERS = [int(lazrenamers) if id_pattern.search(lazrenamers) else lazrenamers for lazrenamers in environ.get('LAZY_RENAMERS', '').split()]
+REQ_CHANNEL = int(environ.get('REQ_CHANNEL', 0))
 
 
 # Auto Delete For Group Message (Self Delete) #
